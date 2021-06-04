781e9260a754694aca267015a47dd0bfa11dc310
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-06-04 17:44:33 +0900

    add link

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,16 +1,21 @@
+from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.template.response import TemplateResponse
 from blog.models import Post
 
+
 def post_list(request):
     return TemplateResponse(
-        request, "post_list.html",
-        {"posts": Post.objects.all()}
+        request,
+        "post_list.html",
+        {"posts": Post.objects.all()},
     )
 
 
 def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
     return TemplateResponse(
-        request, "post_list.html",
-        {"posts": Post.objects.all()}
+        request,
+        "post_detail.html",
+        {"post": post},
     )
